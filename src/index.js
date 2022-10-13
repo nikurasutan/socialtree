f29c420403516d024f1a6b311d6ae28aaa59092d
Nikurasu <publicmail@nikurasu.de>
2022-10-13 15:59:02 +0200

    Inital Commit OwO

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import * as eta from 'eta'
+
+const app = express()
+const port = 3000
+const engine = 'eta'
+
+app.engine(engine, eta.renderFile)
+app.set('view engine', engine)
+app.set('views', './src/views')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => console.log(`App running on port ${port}, with view engine ${engine}`))
