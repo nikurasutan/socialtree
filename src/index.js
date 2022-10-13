7904ba8eba7a7cfb4cf74a499119e8e1ffcbcde8
nikurasu <publicmail@nikurasu.gay>
2022-10-13 21:59:07 +0200

    Switch to ejs for templating becasue of better ide support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import express from 'express'
-import * as eta from 'eta'
 
 const app = express()
 const port = 3000
-const engine = 'eta'
+const engine = 'ejs'
 
-app.engine(engine, eta.renderFile)
 app.set('view engine', engine)
 app.set('views', './src/views')
 
 app.get('/', (req, res) => {
     res.render('index', {
-        answer: 42
+        engine
     })
 })
 
