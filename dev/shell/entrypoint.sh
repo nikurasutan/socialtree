0682dbc3be69fa566fd387eac86e1dff2d808a58
Nikurasu <publicmail@nikurasu.gay>
2023-01-11 21:57:09 +0100

    Removed config file and added example config file

--- dev/shell/entrypoint.sh
+++ dev/shell/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [[ ! -d "/app/assets/config" ]] && [[ ! -d "/app/assets/img" ]]; then
     echo "No config found, installing default files"
+    mkdir /app/assets
     cp -R /assets/* /app/assets
 fi
 
