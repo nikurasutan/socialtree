f58c5e7fa1afbeb683fe2382e96e8742c55dda66
Nikurasu <publicmail@nikurasu.gay>
2023-01-14 14:58:57 +0100

    Update prod build script

--- dev/shell/docker-prod.sh
+++ dev/shell/docker-prod.sh
@@ -1,1 +1,2 @@
 docker build -t dev.cat-enby.club/nikurasu/socialtree-js:latest -f dev/docker/images/main/Dockerfile --no-cache .
+docker build -t dev.cat-enby.club/nikurasu/socialtree-js:$1 -f dev/docker/images/main/Dockerfile .
