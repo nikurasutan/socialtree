ed3de776b09139c38a2b39fb6ac0dedc1fc078b8
nikurasu <publicmail@nikurasu.gay>
2023-03-03 15:01:54 +0100

    Set correct image tag in docker build-script

--- dev/shell/docker-prod.sh
+++ dev/shell/docker-prod.sh
@@ -1,2 +1,2 @@
-docker build -t dev.cat-enby.club/nikurasu/socialtree-js:latest -f dev/docker/images/main/Dockerfile --no-cache .
-docker build -t dev.cat-enby.club/nikurasu/socialtree-js:$1 -f dev/docker/images/main/Dockerfile .
+docker build -t dev.cat-enby.club/nikurasu/socialtree:latest -f dev/docker/images/main/Dockerfile --no-cache .
+docker build -t dev.cat-enby.club/nikurasu/socialtree:$1 -f dev/docker/images/main/Dockerfile .
